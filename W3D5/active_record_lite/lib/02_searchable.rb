4f90492c9a994e73db09f224ba41d29a1f13ba5c
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-06-16 15:56:13 -0700

    searchable works

--- W3D5/active_record_lite/lib/02_searchable.rb
+++ W3D5/active_record_lite/lib/02_searchable.rb
@@ -15,22 +15,6 @@ module Searchable
   end
 end
 
-# def where(params)
-#   where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
-#
-#   results = DBConnection.execute(<<-SQL, *params.values)
-#     SELECT
-#       *
-#     FROM
-#       #{table_name}
-#     WHERE
-#       #{where_line}
-#   SQL
-#
-#   parse_all(results)
-# end
-# end
-
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
