33cb9bea16a89e1bc7aed56da3cc8c4545fb0757
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-06-16 14:24:14 -0700

    more passing specs

--- W3D5/active_record_lite/lib/00_attr_accessor_object.rb
+++ W3D5/active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    # going through each of the names provided
+    names.each do |name|
+      define_method(name) do
+        #getting the name and setting that to get
+        instance_variable_get("@#{name}")
+      end
+      define_method("#{name}=") do |value|
+        # accessing the value of the variable and storing it. 
+        instance_variable_set("@#{name}", value)
+      end
+    end
   end
 end
--- W3D5/active_record_lite/lib/01_sql_object.rb
+++ W3D5/active_record_lite/lib/01_sql_object.rb
@@ -1,46 +1,79 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    found_col = DBConnection.execute2(<<-SQL)
+    SELECT *
+    FROM #{self.table_name}
+    LIMIT 0
+    SQL
+    @columns = found_col.first.map &:to_sym
   end
 
   def self.finalize!
+    self.columns.each do |col_name|
+      define_method(col_name) do
+        self.attributes[col_name]
+      end
+
+      define_method("#{col_name}=") do |value|
+        self.attributes[col_name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
-    # ...
+    all = DBConnection.execute(<<-SQL)
+      SELECT #{table_name}.*
+      FROM #{table_name}
+    SQL
+    parse_all(all)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |result| self.new(result) }
   end
 
   def self.find(id)
-    # ...
+    all = DBConnection.execute(<<-SQL, id)
+      SELECT #{table_name}.*
+      FROM #{table_name}
+      WHERE #{table_name}.id = ?
+    SQL
+    parse_all(all).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+     attr_name = attr_name.to_sym
+     if self.class.columns.include?(attr_name)
+       self.send("#{attr_name}=", value)
+     else
+       raise "unknown attribute '#{attr_name}'"
+     end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
+    # @attributes
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |attr| self.send(attr) }
   end
 
   def insert
