99c172e1dc6c521805738ed7bd1280a837f1bec6
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-06-22 14:13:00 -0700

    adding users controller logic

--- W4D4/MusicApp/app/controllers/users_controller.rb
+++ W4D4/MusicApp/app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
 
   def show
-    
+    render :show
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      UserMailer.activation_email(@user).deliver_now!
+      flash[:notice] =
+        "Successfully created your account! Check your inbox for an activation email."
+      redirect_to new_session_url
+    else
+      flash.new[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
-  def activate
-  end
+  # def activate
+  #   @user = User.find_by_activation_token(params[:activation_token])
+  #   @user.activate!
+  #   login_user!(@user)
+  #   flash[:notice] = "Successfully activated your account!"
+  #   redirect_to root_url
+  # end
 
 
   private
