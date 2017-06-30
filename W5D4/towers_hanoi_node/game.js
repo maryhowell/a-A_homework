c314bc8929afc353ac684670bf6a19829697ff0b
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-06-29 17:17:28 -0700

    added isValidMove and isWon

--- W5D4/towers_hanoi_node/game.js
+++ W5D4/towers_hanoi_node/game.js
@@ -19,11 +19,35 @@ class Game {
     // (2) asks the user where they want to move a disc to/from.
     // Pass the startTowerIdx and endTowerIdx to a callback.
     reader.question("Enter a starting tower: ", start => {
-
-
+       const startTowerIdx = parseInt(start);
+      reader.question("Enter an ending tower: ", end => {
+        const endTowerIdx = parseInt(end);
+        callback(startTowerIdx, endTowerIdx);
+      });
     });
   }
 
+
+  isValidMove(startTowerIdx, endTowerIdx) {
+    // that you can move the top of startTowerIdx
+    // onto the top of endTowerIdx.
+    const startTower = this.towers[startTowerIdx];
+    const endTower = this.towers[endTowerIdx];
+    if (startTower.length === 0){
+      return false;
+    } else if (endTower.length ===0) {
+      return true;
+    } else {
+        const topStartDisc = startTower[startTower.length - 1];
+        const topEndDisc = endTower[endTower.length - 1];
+        return topStartDisc < topEndDisc;
+    }
+
+  }
+
+  isWon() {
+     // move all the discs to the last or second tower
+     return (this.towers[2].length == 3) || (this.towers[1].length == 3);
 
 }
 
