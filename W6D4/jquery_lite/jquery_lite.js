d3da0174987ce1b45521f410059c1a49ddf21d52
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-06 16:43:24 -0700

    adding W6D4

---
+++ W6D4/jquery_lite/jquery_lite.js
@@ -0,0 +1,270 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const DomNodeCollection = __webpack_require__(3);
+
+const _docReadyCallbacks = [];
+let _docReady = false;
+
+window.$l = arg => {
+  switch(typeof(arg)){
+    case "function":
+      return registerDocReadyCallback(arg);
+    case "string":
+      return getNodesFromDom(arg);
+    case "object":
+      if(arg instanceof HTMLElement){
+        return new DomNodeCollection([arg]);
+      }
+  }
+};
+
+$l.extend = (base, ...otherObjs) => {
+  otherObjs.forEach( obj => {
+    for(let prop in obj){
+      base[prop] = obj[prop];
+    }
+  });
+  return base;
+};
+
+$l.ajax = options => {
+  const request = new XMLHttpRequest();
+  const defaults = {
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    method: "GET",
+    url: "",
+    success: () => {},
+    error: () => {},
+    data: {},
+  };
+  options = $l.extend(defaults, options);
+  options.method = options.method.toUpperCase();
+
+  if (options.method === "GET"){
+    //data is query string for get
+    options.url += "?" + toQueryString(options.data);
+  }
+
+  request.open(options.method, options.url, true);
+  request.onload = e => {
+    //NB: Triggered when request.readyState === XMLHttpRequest.DONE ===  4
+    if (request.status === 200) {
+      options.success(request.response);
+    } else {
+      options.error(request.response);
+    }
+  };
+
+  request.send(JSON.stringify(options.data));
+};
+
+//helper methods
+toQueryString = obj => {
+  let result = "";
+  for(let prop in obj){
+    if (obj.hasOwnProperty(prop)){
+      result += prop + "=" + obj[prop] + "&";
+    }
+  }
+  return result.substring(0, result.length - 1);
+};
+
+registerDocReadyCallback = func => {
+  if(!_docReady){
+    _docReadyCallbacks.push(func);
+  } else {
+    func();
+  }
+};
+
+getNodesFromDom = selector => {
+  const nodes = document.querySelectorAll(selector);
+  const nodes_array = Array.from(nodes);
+  return new DomNodeCollection(nodes_array);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  _docReady = true;
+  _docReadyCallbacks.forEach( func => func() );
+});
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+__webpack_require__(0);
+(function webpackMissingModule() { throw new Error("Cannot find module \"lib/jquery_lite.js\""); }());
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=jquery_lite.js.map
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+__webpack_require__(0);
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+throw new Error("Module parse failed: /Users/Spoonflower/Desktop/jquery_lite/lib/dom_node_collection.js Unexpected token (123:29)\nYou may need an appropriate loader to handle this file type.\n|     const parentNodes = [];\n| \n|     this.each({ parentNode } =>\n|       // we apply 'visited' property to prevent adding duplicate parents\n|       parentNode.visited ? parentNodes.push(parentNode) : parentNode.visited = true;");
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=jquery_lite.js.map
