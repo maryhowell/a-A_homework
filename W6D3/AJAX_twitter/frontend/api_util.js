32f509d4d62de08d457391844f9b7e2d25d3cc10
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-05 20:52:07 -0700

    adding W6D3

---
+++ W6D3/AJAX_twitter/frontend/api_util.js
@@ -0,0 +1,26 @@
+const APIUtil = {
+
+  followUser: id => APIUtil.changeFollowStatus(id, "POST"),
+
+  unfollowUser: id => APIUtil.changeFollowStatus(id, "DELETE"),
+
+  changeFollowStatus: (id, method) => (
+    $.ajax({
+      url: `/users/${id}/follow`,
+      dataType: "json",
+      method
+    })
+  ),
+
+  searchUsers: query => (
+    $.ajax({
+      url: "/users/search",
+      dataType: "json",
+      method: "GET",
+      data: { query }
+    })
+  )
+
+};
+
+module.exports = APIUtil;
