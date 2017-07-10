c572c09f9666f130b9bd8cc9f364a2d98903982b
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-07-10 16:02:11 -0700

    message showing

---
+++ W7D1/react_todo/frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import TodoListContainer from './todo_list/todo_list_container';
+
+const App = () => (
+  <div className="app">
+    <h1>Super Awesome Todo List</h1>
+
+  </div>
+);
+
+export default App;
--- W7D1/react_todo/frontend/reducers/root_reducer.js
+++ W7D1/react_todo/frontend/reducers/root_reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
 import todosReducer from './todos_reducer';
-import stepsReducer from './steps_reducer';
+// import stepsReducer from './steps_reducer';
 
 const RootReducer = combineReducers({
-  todos: todosReducer,
-  steps: stepsReducer
+  todos: todosReducer
+
 });
 
 export default RootReducer;
